468fec26a58c4c0128a8b3df7be7e4d749848664
leandro-sbetancourt <33640368+leandro-sbetancourt@users.noreply.github.com>
2020-10-15 09:45:43 +0100

    Add files via upload

--- rloutrading.py
+++ rloutrading.py
@@ -3,12 +3,12 @@ import pandas as pd
 import xarray as xr
 from numpy.random import randn
 from tqdm import tqdm
-
+                                     # Working version
 class QTrading:
     def __init__(self, kappa, sigma, xbar, phi, gamma, c,
                  T, dt, A, B, C, D, xmin=90, xmax=110, buy_min=-5,
                  buy_max=5, inventory_min=-20, inventory_max=20,
-                 eps_0=0.01):
+                 eps_0=0.01, n_stdev_X=5, n_price=40,n_decimals_time=4,n_decimals_price=4):
         """
         Constructor for the Q-learning program
 
@@ -31,12 +31,13 @@ class QTrading:
         dt: float
             timestep change
         A: int
-
+            used in learning rate
         B: int
-            lera
+            used in learning rate
         C: int
-
+            used in exploration rate
         D: int
+            used in exploration rate        
         """
         self.kappa = kappa
         self.sigma = sigma
@@ -51,23 +52,30 @@ class QTrading:
         self.C = C
         self.D = D
         self.eps_0 = eps_0
+        self.n_decimals_time = n_decimals_time
+        self.n_decimals_price = n_decimals_price
         self.timesteps = self._initialize_timesteps()
+        self.buy_min=buy_min
+        self.buy_max=buy_max
         self.inventory_min = inventory_min
         self.inventory_max = inventory_max
         self.inventory = np.arange(inventory_min, inventory_max + 1)
+        self.n_stdev_X = n_stdev_X
+        self.n_price = n_price
         self.actions = np.arange(buy_min, buy_max + 1)
         self.action_space = self._initialize_df_action()
         self.buckets = self._initialize_price_buckets()
         self._Q = self._initialize_Q_matrix()
+        self.big_n_Q_dimension = len(self.timesteps) * len(self.buckets) * len(self.inventory) * len(self.actions)
+
 
     @property
     def Q(self):
         return self._Q
 
     def _initialize_timesteps(self):
-        #to-do (Leo): Correct rounding feature
         time = np.arange(0, self.T, self.dt)
-        time = np.round(time, 4)
+        time = np.round(time, self.n_decimals_time)
         return time
 
     def _initialize_df_action(self):
@@ -76,12 +84,12 @@ class QTrading:
         return action_space
 
 
-    def _initialize_price_buckets(self, n=5, ndecimals=4):
-        upper_bound = self.xbar + n * self.sigma / np.sqrt(2 * self.kappa)
-        lower_bound = self.xbar - n * self.sigma / np.sqrt(2 * self.kappa)
+    def _initialize_price_buckets(self):
+        upper_bound = self.xbar + self.n_stdev_X * self.sigma / np.sqrt(2 * self.kappa)
+        lower_bound = self.xbar - self.n_stdev_X * self.sigma / np.sqrt(2 * self.kappa)
         bucket_size = self.sigma * np.sqrt(self.dt) / 0.5
-        buckets = np.arange(lower_bound, upper_bound, bucket_size).round(ndecimals)
-        buckets = np.linspace(lower_bound, upper_bound, 40)
+        buckets = np.arange(lower_bound, upper_bound, bucket_size).round(self.n_decimals_price)
+        buckets = np.linspace(lower_bound, upper_bound, self.n_price).round(self.n_decimals_price)
 
         return buckets
 
@@ -93,11 +101,45 @@ class QTrading:
         n_actions = len(self.actions)
 
         state_space = n_timesteps, n_bucket_prices, n_inventory, n_actions
-        Q = np.random.rand(*state_space) / 1000
+        Q = np.random.rand(*state_space) / 1000000
+        
+        aposs=self.actions
+        
+        for it in range(len(self.timesteps)):
+            for q in range(n_inventory):
+                mask = np.ones(aposs.shape,dtype=bool)
+                poss = self.get_possible_actions_zero_inventory_end(it,self.inventory[q])-self.buy_min
+                mask[poss] = False
+
+                Q[it,:,q,mask]=-np.inf
+                
         return Q
 
 
+    
+    def get_possible_actions_zero_inventory_end(self,it, q):
+        
+        if it==(len(self.timesteps)-1):
+            possible_actions = np.array([-q])
+            possible_actions = np.clip(possible_actions, (self.buy_min) , (self.buy_max) ) 
+            return possible_actions
+        elif it==(len(self.timesteps)-2):
+            locate_q = self.action_space[(self.inventory==q).argmax(),:]
+            idx = ((self.buy_min <=  locate_q) &
+                       (locate_q <= self.buy_max))
+            possible_actions = locate_q[idx] - q 
+            return possible_actions
+        else:
+            locate_q = self.action_space[(self.inventory==q).argmax(),:]
+            idx = ((self.inventory_min <=  locate_q) &
+                       (locate_q <= self.inventory_max))
 
+
+            possible_actions = locate_q[idx] - q 
+            return possible_actions
+
+    
+    
     def simulate_ou_process(self, x0=None, nsims=1, random_shock=False):
         if random_shock == True:
             x0 = np.random.choice(self.buckets)
@@ -113,7 +155,7 @@ class QTrading:
             x[t + 1,:] = (x[t,:] + self.dt * (self.kappa * (self.xbar - x[t,:]))
                          + np.sqrt(self.dt) * self.sigma * errs[t,:])
 
-        x = np.clip(x, self.buckets.min(), self.buckets.max())
+        x = np.clip(x, self.buckets.min()+1e-10, self.buckets.max()-1e-10)
         return x
 
 
@@ -145,7 +187,6 @@ class QTrading:
 
     def get_position_indices(self, t, price=None, inventory=None, action=None):
         """
-        **Check time overhead
         (time, bucket_index, inventory_index, action_index)
         """
         if inventory is None:
@@ -173,14 +214,14 @@ class QTrading:
 
 
     def run_episode(self, iteration, random_shock=False):
-        Xt, R = self.simulate_reward_matrix(random_shock=False)
+        Xt, R = self.simulate_reward_matrix(random_shock=random_shock)
         Xt = self.buckets[np.digitize(Xt, self.buckets)]
-        q = 0
+        q = 0 #np.random.choice(self.inventory)
         for it, t in enumerate(self.timesteps[:-1]):
             xt = Xt[it]
             xt_prime = Xt[it + 1]
-            action, q_prime, Q_update_value = self.step_in_episode(R, t, xt, xt_prime, q, iteration)
-            selection_current = self.get_position_indices(it, xt, q, action)
+            action, q_prime, Q_update_value = self.step_in_episode(R, it, t, xt, xt_prime, q, iteration)
+            selection_current = self.get_position_indices(t, xt, q, action) #error it had an "it" instead of t
             self.Q[selection_current] = Q_update_value
             q = q_prime
 
@@ -190,7 +231,7 @@ class QTrading:
         alpha_k = self.learning_rate(iteration)
 
         i_t, _, i_q, i_action = self.get_position_indices(t=t, inventory=q_prime,
-            action=action)
+            action=action)     
         reward = R[i_t, i_q, i_action]
 
         selection_current = self.get_position_indices(t=t, inventory=q, price=xt, action=action)
@@ -205,7 +246,7 @@ class QTrading:
         return Q_update_value
 
 
-    def step_in_episode(self, R, t, xt, xt_prime, q, iteration):
+    def step_in_episode(self, R, it, t, xt, xt_prime, q, iteration):
         """
 
         Paramters
@@ -220,7 +261,7 @@ class QTrading:
         eps_k = self.exploration_rate(iteration)
         eps_k = max(eps_k, self.eps_0)
         pr = np.random.rand()
-        possible_actions = self.get_possible_actions(q)
+        possible_actions = self.get_possible_actions_zero_inventory_end(it,q)
         if pr < eps_k:
             new_action = np.random.choice(possible_actions)
         else:
@@ -232,7 +273,7 @@ class QTrading:
             new_action = self.actions[new_action_ix]
 
         q_prime = q + new_action
-        t_prime = np.round(t + self.dt, 4)
+        t_prime = np.round(t + self.dt, self.n_decimals_time)
 
 
         Q_update_value = self.get_Q_update_value(R, t, t_prime, xt, xt_prime, q,
@@ -242,9 +283,6 @@ class QTrading:
 
 
     def q_learn(self, n_iterations, random_shock=False):
-        """
-        to-do: implement
-        """
         try:
             for iteration in tqdm(range(n_iterations)):
                 try:
